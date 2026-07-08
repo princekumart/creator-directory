cc0e8a0395c923815cc7a088e92de0631211ec11
Prince Kumar Tiwari <princekumart810@gmail.com>
2026-07-08 19:43:51 +0530

    Update backend API URL

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4001",
+  baseURL: "https://creator-directory-backend.onrender.com",
 });
 
 export default api;
